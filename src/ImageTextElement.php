cf00d84ce145457ae9e21717733c6be4b31b048a
Tim <DorsetDigital@users.noreply.github.com>
2026-09-19 15:37:16 +0200

    Modernise image and link relations

--- src/ImageTextElement.php
+++ src/ImageTextElement.php
@@ -1,48 +1,57 @@
 <?php
+
 namespace DorsetDigital\Elements;
 
-use Override;
 use DNADesign\Elemental\Models\BaseElement;
+use Override;
 use SilverStripe\AssetAdmin\Forms\UploadField;
 use SilverStripe\Assets\Image;
-use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Forms\DropdownField;
 use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
 use SilverStripe\Forms\TextField;
-use SilverStripe\Forms\TreeDropdownField;
-
+use SilverStripe\LinkField\Form\LinkField;
+use SilverStripe\LinkField\Models\Link;
 
 class ImageTextElement extends BaseElement
 {
-
     private static $singular_name = 'Text & Image Block';
     private static $plural_name = 'Text & Image Blocks';
     private static $class_description = 'Adds a block of text with accompanying image';
     private static $table_name = 'DorsetDigital_Elements_ImageText';
+
     private static $db = [
         'Content' => 'HTMLText',
         'ImagePosition' => 'Varchar(10)',
         'ImageAlt' => 'Varchar(255)',
-        'ImageWidth' => 'Varchar(10)'
+        'ImageWidth' => 'Varchar(10)',
     ];
-    private static $many_many = [
-        'Image' => Image::class
-    ];
-    private static $owns = [
-        'Image'
-    ];
-    private static $inline_editable = false;
+
     private static $has_one = [
-        'ImageLink' => SiteTree::class
+        'Image' => Image::class,
+        'ImageLink' => Link::class,
     ];
+
+    private static $owns = [
+        'Image',
+        'ImageLink',
+    ];
+
+    private static $cascade_deletes = [
+        'ImageLink',
+    ];
+
+    private static $cascade_duplicates = [
+        'ImageLink',
+    ];
+
+    private static $inline_editable = false;
 
     private static $sizes = [
         'half' => '1/2 page width',
         'third' => '1/3 page width',
         'quarter' => '1/4 page width',
-        'sixth' => '1/6 page width'
+        'sixth' => '1/6 page width',
     ];
-
 
     #[Override]
     public function getCMSFields()
@@ -55,16 +64,15 @@ class ImageTextElement extends BaseElement
                 ->setFolderName('pageimages')
                 ->setAllowedMaxFileNumber(1),
             DropdownField::create('ImagePosition')
-                ->setSource([ 'after' => 'After Content', 'before' => 'Before Content' ]),
+                ->setSource(['after' => 'After Content', 'before' => 'Before Content']),
             TextField::create('ImageAlt')->setTitle('Alt text for the image'),
             DropdownField::create('ImageWidth')
                 ->setSource($this->config()->get('sizes'))
-                ->setDescription('Relative image size on larger screens.  On smaller screens the image will flow before or after the text content, instead of sitting next to it.'),
-            TreeDropdownField::create('ImageLinkID',
-                _t(self::class . '.Link', 'Link'),
-                SiteTree::class
-            )->setDescription(_t(self::class . '.LinkDescription', 'Optional link for the image'))
+                ->setDescription('Relative image size on larger screens. On smaller screens the image will flow before or after the text content, instead of sitting next to it.'),
+            LinkField::create('ImageLink', _t(self::class . '.Link', 'Link'))
+                ->setDescription(_t(self::class . '.LinkDescription', 'Optional link for the image')),
         ]);
+
         return $fields;
     }
 
@@ -73,5 +81,4 @@ class ImageTextElement extends BaseElement
     {
         return 'Image & Text';
     }
-
 }
